043827fc2703963f83a77be3f584b921f2abe160
Mahmoudwahdan1996 <mw01007928381@gmail.com>
2022-03-30 00:50:31 +0200

    add mongo url

--- utils/helper/db.js
+++ utils/helper/db.js
@@ -1,9 +1,7 @@
 import { MongoClient } from "mongodb";
 
 export async function connectToDatabase() {
-  const client = await MongoClient.connect(
-    "mongodb+srv://MH_db1996:mh221096@cluster0.mjypt.mongodb.net/auth-demo?retryWrites=true&w=majority"
-  );
+  const client = await MongoClient.connect(`${process.env.MONGO_URL}`);
 
   return client;
 }
